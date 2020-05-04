a2cbcae3618058ca14dd3be6366d4ad606507887
Mark Maher <mark.j.maher25@gmail..com>
2020-05-04 16:07:28 -0500

    Added a very badic _app.jsx file

---
+++ pages/_app.jsx
@@ -0,0 +1,6 @@
+import "../styles/main.scss";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
