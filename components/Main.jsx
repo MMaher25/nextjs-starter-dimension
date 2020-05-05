ac0ff2df90a41784c8188af590d160237231bcb5
Mark Maher <mark.j.maher25@gmail..com>
2020-05-04 20:49:50 -0500

    Updated from deprecated skel to replacement Responsive Tools

--- components/Main.jsx
+++ components/Main.jsx
@@ -115,21 +115,23 @@ const Main = ({
     >
       <h2 className="major">Contact</h2>
       <form method="post" action="#">
-        <div className="field half first">
-          <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" />
-        </div>
-        <div className="field half">
-          <label htmlFor="email">Email</label>
-          <input type="text" name="email" id="email" />
-        </div>
-        <div className="field">
-          <label htmlFor="message">Message</label>
-          <textarea name="message" id="message" rows="4"></textarea>
+        <div className="fields">
+          <div className="field half first">
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" id="name" />
+          </div>
+          <div className="field half">
+            <label htmlFor="email">Email</label>
+            <input type="text" name="email" id="email" />
+          </div>
+          <div className="field">
+            <label htmlFor="message">Message</label>
+            <textarea name="message" id="message" rows="4"></textarea>
+          </div>
         </div>
         <ul className="actions">
           <li>
-            <input type="submit" value="Send Message" className="special" />
+            <input type="submit" value="Send Message" className="primary" />
           </li>
           <li>
             <input type="reset" value="Reset" />
